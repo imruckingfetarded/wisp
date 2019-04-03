9127ba2eb3499cb2f48de278e0315106db305164
imruckingfetarded <49215868+imruckingfetarded@users.noreply.github.com>
2019-04-02 19:49:02 -0500

    Update setPrefix.js

--- commands/setPrefix.js
+++ commands/setPrefix.js
@@ -6,7 +6,19 @@ module.exports.run = async (bot, message, args, moment, db) => {
         if (q.exists) {
             if (q.data().guildOwnerID != message.author.id) {
                 message.channel.send("You must be the owner of the server in-order to change the server prefix!");
-            }
+                return;
+            } else if(args.length === 0) {
+                message.channel.send("Need a prefix to change!")
+             } else if (args.length === 1) {
+                 let nPre = args[0];
+        
+                 db.collection('guilds').doc(message.guild.id).update({
+                     'prefix' : nPre
+                 }).then(() => {
+                    message.channel.send(`Prefix has been set to ${nPre}`);
+                 });
+                 
+             }
         }
     });
     
@@ -14,18 +26,7 @@ module.exports.run = async (bot, message, args, moment, db) => {
     
 
 
-    if(args.length === 0) {
-        message.channel.send("Need a prefix to change!")
-     } else if (args.length === 1) {
-         let nPre = args[0];
-
-         db.collection('guilds').doc(message.guild.id).update({
-             'prefix' : nPre
-         }).then(() => {
-            message.channel.send(`Prefix has been set to ${nPre}`);
-         });
-         
-     }
+    
     
 
 
