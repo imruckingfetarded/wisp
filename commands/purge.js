419bcfc59e48cb37a50da44e56e266af0951f8e4
imruckingfetarded <49215868+imruckingfetarded@users.noreply.github.com>
2019-04-03 16:14:19 -0500

    two new commands

---
+++ commands/purge.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args, moment) => {
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("");
+    if(args.length < 1) return message.channel.send("❌ Please specify the amount of messages you want to purge!");
+
+    if(args.length <= 5) {
+        message.channel.send("❌ You may not purge more than 9,999 messages!")
+    } else {
+
+    message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send("✅  `" + `${args[0]}` + "` messages have been purged!").then(msg => msg.delete(2000));
+  });
+    }
+
+}
+
+module.exports.config = {
+    name: "say",
+    aliases: ["s"]
+}
---
+++ commands/say.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args, moment) => {
+message.delete();
+
+if (args.length < 0) return channel.message.send("Are you going to tell me what to say?")
+    let theMessage = args.join(" ")
+message.channel.send(theMessage)
+
+}
+
+module.exports.config = {
+    name: "say",
+    aliases: ["s"]
+}
